9f713649bd8951a20bb29699ef7f8bcadecf1e59
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-04-11 22:30:35 -0300

    New commit: changes in hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Polaroid from "../img/Polaroid.PNG";
+import Circle from "../img/Circle.PNG";
 
 const Hero = () => {
   return (
     <section className="hero">
-      <h1>
-        I'm a <strong>Front-end Developer</strong> &{"\n"}
-        <strong>English-Spanish Translator</strong> & {"\n"}
-        <strong>Digital Artist</strong>
-      </h1>
+      <div className="title">
+        <h1>
+          I'm a <strong>Front-end Developer</strong> &{"\n"}
+          <strong>English-Spanish Translator</strong> & {"\n"}
+          <strong>Digital Artist</strong>
+        </h1>
+        <img className="circle-img" src={Circle} alt="" />
+      </div>
       <figure>
-        <img src={Polaroid} alt="" />
+        <img
+          src={Polaroid}
+          alt="Polaroid selfie with the description 'my portfolio'"
+        />
       </figure>
     </section>
   );
