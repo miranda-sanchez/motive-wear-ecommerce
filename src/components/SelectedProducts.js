447e0a1ee579128a4cfd6f0ef801fc0343fdefe9
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-08-08 22:25:42 -0300

    Add: changes in design

--- src/components/SelectedProducts.js
+++ src/components/SelectedProducts.js
@@ -1,25 +1,55 @@
 import React from "react";
+import Slider from "react-slick";
 import { products } from "../data/ProductData";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SelectedProducts = () => {
   const bestSellers = products.filter((product) => product.bestSeller);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0",
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <section className="SelectedProducts">
       <h2>Popular Products</h2>
       <div className="cards-container">
-        {bestSellers.map((product) => (
-          <article key={product.id} className="card">
-            <figure>
-              <img src={product.urlImg} alt={product.nameProduct} />
-            </figure>
-            <div className="info-card">
-              <p className="shipping">Free shipping</p>
-              <h3>{product.nameProduct}</h3>
-              <p className="price">${product.price}</p>
-            </div>
-          </article>
-        ))}
+        <Slider {...settings}>
+          {bestSellers.map((product) => (
+            <article key={product.id} className="card">
+              <figure>
+                <img src={product.urlImg} alt={product.nameProduct} />
+              </figure>
+              <div className="info-card">
+                <p className="shipping">Free shipping</p>
+                <h3>{product.nameProduct}</h3>
+                <p className="price">${product.price}</p>
+              </div>
+            </article>
+          ))}
+        </Slider>
       </div>
     </section>
   );
